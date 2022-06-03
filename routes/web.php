cdd00c5e11a1066aa659fff3a40962d482ee30d4
AsiraKolae <asirakolae31@gmail.com>
2022-06-02 22:39:37 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,33 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/day_report', function (){
+    return view('day_report');
+});
+
+// Route::get('/profile', function (){
+//     return view('employee_details');
+// });
+
+Route::get('/day', function (){
+    return App\DayReport::all();
+});
+
+Route::get('employee', function (){
+    return view('employee');
+});
+
+Route::get('/reports',[App\Http\Controllers\ReportsController::class,'index']);
+
+Route::get('/employee',[App\Http\Controllers\EmployeeController::class,'index']);
+
+Route::middleware(['auth:sanctum'.'verified'])->get('/dashboard',function(){
+    $employee=DB::table('employee')->get();
+    return view('dashboard',compact('users'));
+});
+
+Route::get('/employee_details/{em_id}', [App\Http\Controllers\EmployeeDetailsController::class, 'EmployeeDetails']);
+
+Route::get('/chart/day', [App\Http\Controllers\ChartController::class, 'Chart_Day']);
+
